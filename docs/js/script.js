2e809f40c4d1441c1f7eb77e57604f47b4e51305
Vlad <vlad.ustupniy@yandex.ru>
2023-07-09 16:28:13 +0300

    Добавил слайдеры в остальные секции

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,45 +1,21 @@
-/* const clientsSLiderItem = document.querySelectorAll('.clients__item');
-window.addEventListener('resize', () => {
-    if (this.window.innerWidth <= 1200) {
-        clientsSLiderItem[3].style.display = "none";
-    }else{
-        clientsSLiderItem[3].style.display = "block";
-    }
-
-    if (this.window.innerWidth <= 992) {
-        clientsSLiderItem[2].style.display = "none";
-    }else{
-        clientsSLiderItem[2].style.display = "block";
-    }
-
-    if (this.window.innerWidth <= 768) {
-        clientsSLiderItem[1].style.display = "none";
-    }else{
-        clientsSLiderItem[1].style.display = "block";
-    }
-}); */
-
-
-
 function disableScroll() {
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    window.onscroll = function() {
-        window.scrollTo(scrollLeft, scrollTop);
-    };
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+    (window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    });
 }
 function enableScroll() {
-    window.onscroll = function() {};
+  window.onscroll = function () {};
 }
 
-
-const burger = document.getElementById('burger');
-const navLine = document.getElementById('navLine');
-burger.addEventListener('click', () => {
-    navLine.classList.toggle('nav__line--active');
-    if (navLine.classList.contains('nav__line--active')) {
-        disableScroll();
-    }else{
-        enableScroll();
-    };
+const burger = document.getElementById("burger");
+const navLine = document.getElementById("navLine");
+burger.addEventListener("click", () => {
+  navLine.classList.toggle("nav__line--active");
+  if (navLine.classList.contains("nav__line--active")) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
 });
--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-      // часто используемые:
-      items: 1, // количество слайдов на экране
-      loop: true, // бесконечная прокрутка
-      margin: 20, // расстояние между слайдами
-      nav: true, // стрелки
-      dots: true, // точки
-      mouseDrag: true, // перелистывание мышкой
-      touchDrag: true, // перелистывание на тачскринах
-      navText: [
-        '<img src="img/arrow-prev.svg" alt="Назад">',
-        '<img src="img/arrow-next.svg" alt="Вперёд">'
-      ], // текст кнопок слайдера?
-      // responsive:{0:{items:1},600:{items:3},1000:{items:5}} // адаптив
-  
-      // редко используемые:
-      center: false, // центрирование слайда
-      autoWidth: false, // автоматическая ширина всего слайдера
-      rewind: false, // перемотка назад после завершения пролистывания
-      lazyLoad: false, // ленивая загрузка
-      video: false, // видео
-      videoWidth: false, // ширина видео
-      videoHeight: false, // высота видео
-      autoplay: false, // автопролистывание
-      autoplayTimeout: 5000, // Автоматическое отключение воспроизведения
-      autoplayHoverPause: false, // Автозапуск с остановкой при ховере
-    });
+  $(".owl-carousel").owlCarousel({
+    // часто используемые:
+    items: 1, // количество слайдов на экране
+    loop: true, // бесконечная прокрутка
+    margin: 0, // расстояние между слайдами
+    nav: true, // стрелки
+    dots: true, // точки
+    mouseDrag: true, // перелистывание мышкой
+    touchDrag: true, // перелистывание на тачскринах
+    navText: [
+      '<img src="img/arrow-prev.svg" alt="Назад">',
+      '<img src="img/arrow-next.svg" alt="Вперёд">',
+    ], // текст кнопок слайдера?
+    // responsive:{0:{items:1},600:{items:3},1000:{items:5}} // адаптив
+
+    // редко используемые:
+    center: false, // центрирование слайда
+    autoWidth: false, // автоматическая ширина всего слайдера
+    rewind: false, // перемотка назад после завершения пролистывания
+    lazyLoad: false, // ленивая загрузка
+    video: false, // видео
+    videoWidth: false, // ширина видео
+    videoHeight: false, // высота видео
+    autoplay: false, // автопролистывание
+    autoplayTimeout: 5000, // Автоматическое отключение воспроизведения
+    autoplayHoverPause: false, // Автозапуск с остановкой при ховере
   });
+});
