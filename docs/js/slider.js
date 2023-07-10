27590e3050c01c23eb131b02ee7ee2f92ff7c84b
Vlad <vlad.ustupniy@yandex.ru>
2023-07-10 21:46:27 +0300

    Доделал слайдер на 4 элемента в секции 'Clients'

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    // часто используемые:
     items: 1, // количество слайдов на экране
     loop: true, // бесконечная прокрутка
     margin: 0, // расстояние между слайдами
@@ -11,19 +10,26 @@ $(document).ready(function () {
     navText: [
       '<img src="img/arrow-prev.svg" alt="Назад">',
       '<img src="img/arrow-next.svg" alt="Вперёд">',
-    ], // текст кнопок слайдера?
-    // responsive:{0:{items:1},600:{items:3},1000:{items:5}} // адаптив
+    ],
+  });
 
-    // редко используемые:
-    center: false, // центрирование слайда
-    autoWidth: false, // автоматическая ширина всего слайдера
-    rewind: false, // перемотка назад после завершения пролистывания
-    lazyLoad: false, // ленивая загрузка
-    video: false, // видео
-    videoWidth: false, // ширина видео
-    videoHeight: false, // высота видео
-    autoplay: false, // автопролистывание
-    autoplayTimeout: 5000, // Автоматическое отключение воспроизведения
-    autoplayHoverPause: false, // Автозапуск с остановкой при ховере
+  $(".owl-carousel-two").owlCarousel({
+    items: 4, // количество слайдов на экране
+    loop: true, // бесконечная прокрутка
+    margin: 0, // расстояние между слайдами
+    nav: true, // стрелки
+    dots: true, // точки
+    mouseDrag: true, // перелистывание мышкой
+    touchDrag: true, // перелистывание на тачскринах
+    navText: [
+      '<img src="img/arrow-prev.svg" alt="Назад">',
+      '<img src="img/arrow-next.svg" alt="Вперёд">',
+    ],
+    responsive: {
+      0: { items: 1, slideBy: 1 },
+      768: { items: 2, slideBy: 2 },
+      992: { items: 3, slideBy: 3 },
+      1200: { items: 4, slideBy: 4 },
+    }, // адаптив
   });
 });
